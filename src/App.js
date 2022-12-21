8b887cafa112d875ce7941fb298b635335dfe84b
Ajit <ajitcoder35@gmail.com>
2022-12-21 23:38:29 +0530

    Enable dark mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import About from "./components/About";
 
 
 function App() {
   return (
     <>
-      <Navbar title="Textutils"  about="About section here"   services="service seciton here"/>
+      {/* <Navbar title="Textutils"  about="About section here"   services="service seciton here"/>
       <div className="container">
-      <TextForm title="Enter text below to be Processed...."/>
-      </div>
+      <TextForm title="Enter text below to be Processed...."/> */}
+      {/* </div> */}
+      <About/> 
     </>
   );
 }
